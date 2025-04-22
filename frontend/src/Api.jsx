5dd7da3f45dca7354b4095e167913a0e755694a2
Muriel08 <reptiliana8@outlook.com>
2025-04-21 22:13:56 -0400

    se conectó backend con frontend

--- frontend/src/Api.jsx
+++ frontend/src/Api.jsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000', // Asegurate que sea la URL de tu backend Flask
+  baseURL: 'http://127.0.0.1:5000', // Asegurate que sea la URL de tu backend Flask
 });
 
 export default API;
--- frontend/src/components/ClienteList.jsx
+++ frontend/src/components/ClienteList.jsx
@@ -24,11 +24,12 @@ const ClienteList = ({ onSelect }) => {
       <h2>Lista de Clientes</h2>
       <ul>
         {clientes.map((c) => (
-          <li key={c.ID_Cliente}>
-            {c.Nombre} {c.Apellido} ({c.Email}){' '}
-            <button onClick={() => onSelect(c)}>Editar</button>
-            <button onClick={() => eliminarCliente(c.ID_Cliente)}>Eliminar</button>
-          </li>
+          <li key={c.id}>
+          {c.nombre} ({c.email}){' '}
+          <button onClick={() => onSelect(c)}>Editar</button>
+          <button onClick={() => eliminarCliente(c.id)}>Eliminar</button>
+        </li>
+        
         ))}
       </ul>
     </div>
